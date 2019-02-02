0c1a828ffba7d3a1a5040a1873914e6c23345d22
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-02-02 14:14:15 -0500

    added Dashboard component added class and render function to render as a presentational component

---
+++ atbatapp/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  // constructor() {
+  //
+  // }
+
+  render() {
+    return (
+      <div>
+        <button>Strike</button>
+        <button>Ball</button>
+        <button>Foul</button>
+        <button>Hit</button>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
