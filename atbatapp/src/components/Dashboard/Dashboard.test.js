d1e54a35a372130901f80688f0c71570304d7622
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-02-05 23:27:25 -0500

    added test to check buttons being rendered

--- atbatapp/src/components/Dashboard/Dashboard.test.js
+++ atbatapp/src/components/Dashboard/Dashboard.test.js
@@ -7,5 +7,25 @@ describe('The Dashboard component', () => {
   test('renders the Dashboard component', () => {
     render(<Dashboard />);
   });
-  
+
+  it('renders the strike button', () => {
+    const { getByText, getByTestId } = render(<Dashboard />);
+    const strikeButton = getByTestId('strike-button');
+    getByText(/strike/i);
+  });
+  it('renders the ball button', () => {
+    const { getByText, getByTestId } = render(<Dashboard />);
+    const ballButton = getByTestId('strike-button');
+    getByText(/ball/i);
+  });
+  it('renders the foul button', () => {
+    const { getByText, getByTestId } = render(<Dashboard />);
+    const foulButton = getByTestId('strike-button');
+    getByText(/foul/i);
+  });
+  it('renders the hit button', () => {
+    const { getByText, getByTestId } = render(<Dashboard />);
+    const hitButton = getByTestId('strike-button');
+    getByText(/hit/i);
+  });
 });
