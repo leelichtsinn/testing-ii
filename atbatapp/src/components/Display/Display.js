b6237bb1619a99b5ee083a754fad026ba5373a21
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-02-05 23:26:26 -0500

    refactored to presentational component

--- atbatapp/src/components/Display/Display.js
+++ atbatapp/src/components/Display/Display.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 
-class Display extends Component {
-  // keep track of balls and strikes with state
-  state = {
-    ballCount: 0,
-    strikeCount: 0
-  }
+const Display = props => {
+  const { balls, strikes } = props;
 
-  render() {
-    return (
-      <div>
-        <div>
-          {/* display ballCount from state */}
-          <h2>Balls:</h2>
-          <div>{this.state.ballCount}</div>
-        </div>
-        <div>
-          {/* display strikeCount from state */}
-          <h2>Strikes:</h2>
-          <div>{this.state.strikeCount}</div>
-        </div>
-      </div>
-    )
-  }
+  return (
+    <>
+    <h3>Counts</h3>
+    <div>
+      {balls} balls and {strikes} strikes
+    </div>
+    </>
+  )
 }
 
 export default Display;
+
+// class Display extends Component {
+//   // keep track of balls and strikes with state
+//   state = {
+//     ballCount: 0,
+//     strikeCount: 0
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <div>
+//           {/* display ballCount from state */}
+//           <h2 data-testid='balls title'>Balls:</h2>
+//           <div data-testid='balls count'>{this.state.ballCount}</div>
+//         </div>
+//         <div>
+//           {/* display strikeCount from state */}
+//           <h2 data-testid='strikes title'>Strikes:</h2>
+//           <div data-testid='strikes count'>{this.state.strikeCount}</div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default Display;
